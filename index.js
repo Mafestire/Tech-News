3f855b5a62ad232faf79f6cf9fef45cd75ad11d8
Mafestire <seabaraven@gmail.com>
2023-08-16 12:11:52 +0200

    date update

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
         q: 'coding',
         sources: 'engadget',
         domains: ' techcrunch.com',
-        from: '2023-06-30',
+        from: '2023-08-17',
         to: '2017-12-12',
         language: 'en',
         sortBy: 'relevancy',
